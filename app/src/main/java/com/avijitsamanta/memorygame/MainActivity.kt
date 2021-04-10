c12e13f7feb541c3bf0fadb2c6e37b7a25297c3b
samantaavijit <samantaavijit.2000@gmail.com>
2021-04-10 19:54:48 +0530

    create toolbar icon

--- app/src/main/java/com/avijitsamanta/memorygame/MainActivity.kt
+++ app/src/main/java/com/avijitsamanta/memorygame/MainActivity.kt
@@ -3,7 +3,13 @@ package com.avijitsamanta.memorygame
 import android.animation.ArgbEvaluator
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -25,7 +31,7 @@ class MainActivity : AppCompatActivity(), MemoryBoardAdapter.CardClickListener {
 
     private var boardSize: BoardSize = BoardSize.EASY
 
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,19 +41,99 @@ class MainActivity : AppCompatActivity(), MemoryBoardAdapter.CardClickListener {
         tvNumPairs = findViewById(R.id.tvNumPairs)
         clRoot = findViewById(R.id.clRoot)
 
+        setupBoard()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupBoard() {
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "Medium: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Hard: 6 x 4"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+
         memoryGame = MemoryGame(boardSize)
         adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, this)
         rvBoard.adapter = adapter
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
 
-        tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
-        tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
         tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
     }
 
     override fun onCardClicked(position: Int) {
         updateGameWithFlip(position)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.ui_refresh -> {
+                // setup the game again
+                if (memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog("Quit your current game?", null) {
+                        setupBoard()
+                    }
+                } else {
+                    setupBoard()
+                }
+                return true
+            }
+            R.id.ui_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("InflateParams")
+    private fun showNewSizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+        // show previous selected item
+        when (boardSize) {
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+
+        showAlertDialog("Choose new size", boardSizeView) {
+            // set a new value for the board size
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setupBoard()
+        }
+    }
+
+    private fun showAlertDialog(
+        title: String,
+        view: View?,
+        positiveClickListener: View.OnClickListener
+    ) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK") { _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
     }
 
     @SuppressLint("SetTextI18n")
@@ -67,8 +153,10 @@ class MainActivity : AppCompatActivity(), MemoryBoardAdapter.CardClickListener {
                 ContextCompat.getColor(this, R.color.color_progress_none),
                 ContextCompat.getColor(this, R.color.color_progress_full),
             ) as Int
+
             tvNumPairs.setTextColor(color)
             tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
+
             if (memoryGame.haveWonGame()) {
                 Snackbar.make(clRoot, "You won! Congratulation.", Snackbar.LENGTH_LONG).show()
             }
